3512ca0d0ddc2e6a21f78496cc6c200b176834c2
Ethan-estia <189098806+Ethan-estia@users.noreply.github.com>
2025-05-21 17:41:25 +0200

    test commit

--- src/features.c
+++ src/features.c
@@ -14,3 +14,62 @@
 void helloWorld() {
     printf("Hello World !");
 }
+void dimension (char *source_path) {
+    int width, height, channel_count;
+    unsigned char *data;
+    int resultat = read_image_data(source_path, &data, &width, &height, &channel_count);
+    if (resultat) {
+        printf("dimension: %d, %d", width, height); 
+    }
+    else {
+        printf("ERREUR DE LECTURE FICHIER");
+    }
+}
+void first_pixel(char *source_path){
+    int width, height, channel_count;
+    unsigned char *data;
+    int resultat = read_image_data(source_path, &data, &width, &height, &channel_count);
+    if (resultat) {
+        printf("first_pixel: %d, %d, %d", data[0], data[1], data[2]);
+    }
+    else {
+        printf("ERREUR DE LECTURE FICHIER"); 
+    }
+}
+
+    
+void second_line (char *source_path){
+    int width, height, channel_count ;
+    unsigned char *data;
+    int resultat = read_image_data(source_path, &data, &width, &height, &channel_count);
+    if (resultat) {
+        printf("second_line: %d, %d, %d", data[4464], data[4465], data[4466]);
+    }
+    else {
+        printf("ERREUR DE LECTURE FICHIER"); 
+    }
+}
+
+void tenth_pixel (char *source_path){
+    int width, height, channel_count ;
+    unsigned char *data;
+    int resultat = read_image_data(source_path, &data, &width, &height, &channel_count);
+    if (resultat) {
+        printf("tenth_pixel: %d, %d, %d", data[27], data[28], data[29]);
+    }
+    else {
+        printf("ERREUR DE LECTURE FICHIER"); 
+    }
+}
+
+void print_pixel (char *source_path, int x, int y){
+    int width, height, channel_count ;
+    unsigned char *data;
+    int resultat = read_image_data(source_path, &data, &width, &height, &channel_count);
+    if (resultat) {
+        printf("tenth_pixel: %d, %d, %d", data[(x*3-3)+((y-1)*(width*3))], data[(x*3-2)+((y-1)*(width*3))], data[(x*3-1)+((y-1)*(width*3))]);
+    }
+    else {
+        printf("ERREUR DE LECTURE FICHIER"); 
+    }
+}
